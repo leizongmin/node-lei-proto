29f69592c9c57738bd48e638e3762700aa9b4c3c
Zongmin Lei <leizongmin@gmail.com>
2015-11-03 18:53:10 +0800

    test #2 uint

--- test/test.js
+++ test/test.js
@@ -36,4 +36,27 @@ describe('lei-proto', function () {
     });
   });
 
+  it('#2 - uint', function () {
+    var proto = parseProto([
+      ['a', 'uint', 1],
+      ['b', 'uint', 2],
+      ['c', 'uint', 3],
+      ['d', 'uint', 4],
+      ['e', 'uint', 5],
+      ['f', 'uint', 6]
+    ]);
+    var b = proto.encode(123, 456, 789, 101112, 131415, 1617181920);
+    var c = proto.decode(b);
+    dump(b);
+    dump(c);
+    assert.deepEqual(c, {
+      a: 123,
+      b: 456,
+      c: 789,
+      d: 101112,
+      e: 131415,
+      f: 1617181920
+    });
+  });
+
 });
