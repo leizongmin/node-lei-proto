c296fdf1e376006bcb9c20472432e5b59ee6fe3c
Zongmin Lei <leizongmin@gmail.com>
2015-11-05 10:34:56 +0800

    性能测试程序

--- benchmark/index.js
+++ benchmark/index.js
@@ -7,11 +7,13 @@
 var parseProto = require('../');
 
 
-function test (title, proto, times, fn) {
+function test (title, proto, times, encodeName, decodeName, fn) {
   var p = parseProto(proto);
   var t = Date.now();
+  var encode = p[encodeName];
+  var decode = p[decodeName];
   for (var i = 0; i < times; i++) {
-    fn(p.encode, p.decode, p.encodeEx);
+    fn(encode, decode, p.encodeEx);
   }
   var s = Date.now() - t;
   console.log('%s\n - %s times, total spent %sms, average %sms, %s times/s',
@@ -21,69 +23,80 @@ function test (title, proto, times, fn) {
 
 var TIMES = 1000000; // 100W
 
-test('#1 encode & decode `int`', [['a', 'uint', 1]], TIMES, function (encode, decode) {
-  decode(encode(1));
-});
-test('#2 encode & decode `uint`', [['a', 'int', 1]], TIMES, function (encode, decode) {
-  decode(encode(1));
-});
-test('#3 encode & decode `float`', [['a', 'float']], TIMES, function (encode, decode) {
-  decode(encode(1.2));
-});
-test('#4 encode & decode `double`', [['a', 'double']], TIMES, function (encode, decode) {
-  decode(encode(1.2));
-});
-test('#5 encode & decode `string`', [['a', 'string', 1]], TIMES, function (encode, decode) {
-  decode(encode('a'));
-});
-test('#6 encode & decode `buffer`', [['a', 'buffer', 1]], TIMES, function (encode, decode) {
-  decode(encode(new Buffer('a')));
-});
-test('#7 encode & decode all', [
-  ['a', 'uint', 1],
-  ['b', 'int', 1],
-  ['c', 'float'],
-  ['d', 'double'],
-  ['e', 'string', 1],
-  ['f', 'buffer', 1]
-], TIMES, function (encode, decode) {
-  decode(encode(1, 2, 3.3, 4.4, 'a', new Buffer('b')));
-});
+function suit (encodeName, decodeName, isEx) {
+  var title = encodeName + ' & ' + decodeName;
+  if (isEx) {
+    test('#1 ' + title + ' `int`', [['a', 'uint', 1]], TIMES, encodeName, decodeName, function (encode, decode) {
+      decode(encode({a: 1}));
+    });
+    test('#2 ' + title + ' `uint`', [['a', 'int', 1]], TIMES, encodeName, decodeName, function (encode, decode) {
+      decode(encode({a: 1}));
+    });
+    test('#3 ' + title + ' `float`', [['a', 'float']], TIMES, encodeName, decodeName, function (encode, decode) {
+      decode(encode({a: 1.2}));
+    });
+    test('#4 ' + title + ' `double`', [['a', 'double']], TIMES, encodeName, decodeName, function (encode, decode) {
+      decode(encode({a: 1.2}));
+    });
+    test('#5 ' + title + ' `string`', [['a', 'string', 1]], TIMES, encodeName, decodeName, function (encode, decode) {
+      decode(encode({a: 'a'}));
+    });
+    test('#6 ' + title + ' `buffer`', [['a', 'buffer', 1]], TIMES, encodeName, decodeName, function (encode, decode) {
+      decode(encode({a: new Buffer('a')}));
+    });
+    test('#7 ' + title + ' all', [
+      ['a', 'uint', 1],
+      ['b', 'int', 1],
+      ['c', 'float'],
+      ['d', 'double'],
+      ['e', 'string', 1],
+      ['f', 'buffer', 1]
+    ], TIMES, encodeName, decodeName, function (encode, decode) {
+      decode(encode({
+        a: 1,
+        b: 2,
+        c: 3.3,
+        d: 4.4,
+        e: 'a',
+        f: new Buffer('b')
+      }));
+    });
+  } else {
+    test('#1 ' + title + ' `int`', [['a', 'uint', 1]], TIMES, encodeName, decodeName, function (encode, decode) {
+      decode(encode(1));
+    });
+    test('#2 ' + title + ' `uint`', [['a', 'int', 1]], TIMES, encodeName, decodeName, function (encode, decode) {
+      decode(encode(1));
+    });
+    test('#3 ' + title + ' `float`', [['a', 'float']], TIMES, encodeName, decodeName, function (encode, decode) {
+      decode(encode(1.2));
+    });
+    test('#4 ' + title + ' `double`', [['a', 'double']], TIMES, encodeName, decodeName, function (encode, decode) {
+      decode(encode(1.2));
+    });
+    test('#5 ' + title + ' `string`', [['a', 'string', 1]], TIMES, encodeName, decodeName, function (encode, decode) {
+      decode(encode('a'));
+    });
+    test('#6 ' + title + ' `buffer`', [['a', 'buffer', 1]], TIMES, encodeName, decodeName, function (encode, decode) {
+      decode(encode(new Buffer('a')));
+    });
+    test('#7 ' + title + ' all', [
+      ['a', 'uint', 1],
+      ['b', 'int', 1],
+      ['c', 'float'],
+      ['d', 'double'],
+      ['e', 'string', 1],
+      ['f', 'buffer', 1]
+    ], TIMES, encodeName, decodeName, function (encode, decode) {
+      decode(encode(1, 2, 3.3, 4.4, 'a', new Buffer('b')));
+    });
+  }
+  console.log('-------------');
+}
 
-console.log('--------');
 
-test('#1 encode & decode `int`', [['a', 'uint', 1]], TIMES, function (encode, decode, encodeEx) {
-  decode(encodeEx({a: 1}));
-});
-test('#2 encode & decode `uint`', [['a', 'int', 1]], TIMES, function (encode, decode, encodeEx) {
-  decode(encodeEx({a: 1}));
-});
-test('#3 encode & decode `float`', [['a', 'float']], TIMES, function (encode, decode, encodeEx) {
-  decode(encodeEx({a: 1.2}));
-});
-test('#4 encode & decode `double`', [['a', 'double']], TIMES, function (encode, decode, encodeEx) {
-  decode(encodeEx({a: 1.2}));
-});
-test('#5 encode & decode `string`', [['a', 'string', 1]], TIMES, function (encode, decode, encodeEx) {
-  decode(encodeEx({a: 'a'}));
-});
-test('#6 encode & decode `buffer`', [['a', 'buffer', 1]], TIMES, function (encode, decode, encodeEx) {
-  decode(encodeEx({a: new Buffer('a')}));
-});
-test('#7 encode & decode all', [
-  ['a', 'uint', 1],
-  ['b', 'int', 1],
-  ['c', 'float'],
-  ['d', 'double'],
-  ['e', 'string', 1],
-  ['f', 'buffer', 1]
-], TIMES, function (encode, decode, encodeEx) {
-  decode(encodeEx({
-    a: 1,
-    b: 2,
-    c: 3.3,
-    d: 4.4,
-    e: 'a',
-    f: new Buffer('b')
-  }));
-});
+suit('encode', 'decode');
+suit('encodeEx', 'decode', true);
+suit('encodeStrict', 'decodeStrict');
+suit('encodeExStrict', 'decodeStrict', true);
+
